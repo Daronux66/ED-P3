e8a74bd7129e8b26002aabaf82a71d8169bb7c9c
Jalvad06 <drossm00@estudiantes.unileon.es>
2020-04-19 23:16:44 +0200

    Terminada

--- src/ule/edi/doubleLinkedList/DoubleLinkedImplTest.java
+++ src/ule/edi/doubleLinkedList/DoubleLinkedImplTest.java
@@ -2,6 +2,9 @@ package ule.edi.doubleLinkedList;
 
 import static org.junit.Assert.*;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 import org.junit.*;
 
 import ule.edi.exceptions.EmptyCollectionException;
@@ -94,9 +97,269 @@ public void antesDe() {
 	        	  
 	}
 	
+	@Test (expected = NullPointerException.class)
+	public void testInstertFirstNullExcep() {
+		lv.insertFirst(null);
+	}
+	
 	@Test
-    public void maxRepeated() {
-        
+	public void testInsertFirst() {
+		lv.insertFirst("A");
+		lv.insertFirst("A");
+		Assert.assertEquals("(A A )", lv.toString());
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testInstertLastNullExcep() {
+		lv.insertLast(null);
+	}
+	
+	@Test
+	public void testInsertLast() {
+		lv.insertLast("A");
+		lv.insertLast("A");
+		Assert.assertEquals("(A A )", lv.toString());
+	}
+	
+	@Test (expected = EmptyCollectionException.class)
+	public void testRemoveFirstEmptyExcep() throws EmptyCollectionException {
+		lv.removeFirst();
+	}
+	
+	@Test
+	public void testRemoveFirst() throws EmptyCollectionException {
+		lv.insertFirst("A");
+		lv.insertFirst("A");
+		Assert.assertEquals("A", lv.removeFirst());
+	}
+	
+	@Test (expected = EmptyCollectionException.class)
+	public void testRemoveLastEmptyExcep() throws EmptyCollectionException {
+		lv.removeLast();
+	}
+	
+	@Test
+	public void testRemoveLast() throws EmptyCollectionException {
+		lv.insertLast("A");
+		lv.insertLast("A");
+		Assert.assertEquals("A", lv.removeLast());
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testInsertPosNullExcep() {
+		lv.insertPos(null, 1);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testInsertPosIllegalExcep() {
+		lv.insertPos("A", 0);
+	}
+	
+	@Test
+	public void testInsertPosSizeMenor() {
+		lv.insertPos("W", 2);
+		Assert.assertEquals("(W )", lv.toString());
+		lv.insertPos("A",1);
+		Assert.assertEquals("(A W )", lv.toString());
+		lv.insertPos("B", 2);
+		Assert.assertEquals("(A B W )", lv.toString());
+		lv.insertPos("B", 3);
+		Assert.assertEquals("(A B B W )", lv.toString());
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testInsertBeforeBothNullExcep() {
+		lv.insertBefore(null, null);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testInsertBeforeElemNullExcep() {
+		lv.insertFirst("A");
+		lv.insertBefore(null, "A");
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testInsertBeforeTargetNullExcep() {
+		lv.insertFirst("A");
+		lv.insertBefore("A", null);
+	}
+	
+	@Test (expected = NoSuchElementException.class)
+    public void testInsertBeforeNoSuchElemExcep() {
+		lv.insertFirst("A");
+		lv.insertBefore("A", "B");
+	}
+	
+	@Test
+    public void testInsertBefore() {
+		lv.insertFirst("A");
+		lv.insertFirst("A");
+		lv.insertBefore("B", "A");
+		lv.insertBefore("C", "A");
+		Assert.assertEquals("(B C A A )", lv.toString());
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+    public void testGetElemPosIllegalExcepExceso() {
+		listaConElems.getElemPos(50);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+    public void testGetElemPosIllegalExcepDefecto() {
+		listaConElems.getElemPos(0);
+	}
+	
+	@Test
+    public void testGetElemPos() {
+		lv.insertFirst("A");
+		Assert.assertEquals("A", lv.getElemPos(1));
+		lv.insertFirst("B");
+		Assert.assertEquals("B", lv.getElemPos(1));
+		lv.insertFirst("Q");
+		Assert.assertEquals("B", lv.getElemPos(2));
+	}
+    
+	@Test (expected = NullPointerException.class)
+    public void testGetPosFirstNullExcep() {
+		lv.getPosFirst(null);
+	}
+	
+	@Test (expected = NoSuchElementException.class)
+    public void testGetPosFirstNoSuchElemExcep() {
+		lv.insertFirst("A");
+		lv.getPosFirst("B");
+	}
+    
+	@Test
+    public void testGetPosFirst() {
+		lv.insertFirst("A");
+		lv.insertFirst("B");
+		lv.insertFirst("B");
+		lv.insertFirst("A");
+		Assert.assertEquals(1, lv.getPosFirst("A"));
+		Assert.assertEquals(2, lv.getPosFirst("B"));
+	}
+	
+	@Test (expected = NullPointerException.class)
+    public void testGetPosLastNullExcep() {
+		lv.getPosLast(null);
+	}
+	
+	@Test (expected = NoSuchElementException.class)
+    public void testGetPosLastNoSuchElemExcep() {
+		lv.insertLast("A");
+		lv.getPosLast("B");
+	}
+    
+	@Test
+    public void testGetPosLast() {
+		lv.insertFirst("A");
+		lv.insertFirst("B");
+		lv.insertFirst("B");
+		lv.insertFirst("A");
+		Assert.assertEquals(4, lv.getPosLast("A"));
+		Assert.assertEquals(3, lv.getPosLast("B"));
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+    public void testRemovePosIllegalExcepDefecto() {
+		lv.insertFirst("A");
+		lv.removePos(0);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+    public void testRemovePosIllegalExcepExceso() {
+		lv.insertFirst("A");
+		lv.removePos(3);
+	}
+	
+	@Test
+    public void testRemovePos() {
+		lv.insertFirst("A");
+		lv.removePos(1);
+		Assert.assertEquals("()", lv.toString());
+		lv.insertFirst("A");
+		lv.insertFirst("A");
+		lv.removePos(1);
+		Assert.assertEquals("(A )", lv.toString());
+		lv.insertFirst("B");
+		lv.removePos(2);
+		Assert.assertEquals("(B )", lv.toString());
+		lv.insertFirst("B");
+		lv.insertFirst("B");
+		lv.removePos(2);
+		Assert.assertEquals("(B B )", lv.toString());		
+	}
+    
+	@Test (expected = NullPointerException.class)
+    public void testRemoveAllNullExcep() {
+		lv.removeAll(null);
+	}
+	
+	@Test (expected = NoSuchElementException.class)
+    public void testRemoveAllNoSuchElemExcep() {
+		lv.insertFirst("A");
+		lv.removeAll("B");
+	}
+	
+	@Test
+    public void testRemoveAll() {
+		int removeCounter;
+		lv.insertFirst("A");
+		removeCounter = lv.removeAll("A");
+		Assert.assertEquals("()", lv.toString());
+		Assert.assertEquals(1, removeCounter);
+		lv.insertFirst("A");
+		lv.insertFirst("A");
+		lv.insertFirst("B");
+		lv.insertFirst("A");
+		removeCounter = lv.removeAll("A");
+		Assert.assertEquals("(B )", lv.toString());
+		Assert.assertEquals(3, removeCounter);
+	}
+    
+	@Test (expected = NullPointerException.class)
+    public void testContainsNullExcep() {
+		lv.contains(null);
+	}
+	
+	@Test
+    public void testContains() {
+		lv.insertFirst("A");
+		Assert.assertTrue(lv.contains("A"));
+		Assert.assertFalse(lv.contains("B"));
+	}
+	
+	@Test
+	public void testSize() {
+		Assert.assertEquals(0, lv.size());
+		lv.insertFirst("A");
+		lv.insertFirst("A");
+		Assert.assertEquals(2, lv.size());
+	}
+	
+	@Test
+	public void testToStringReverse() {
+		Assert.assertEquals("()", lv.toStringReverse());
+		lv.insertFirst("C");
+		lv.insertFirst("B");
+		lv.insertFirst("A");
+		Assert.assertEquals("(C B A )", lv.toStringReverse());
+	}
+	
+	@Test
+	public void testReverse() {
+		lv.insertLast("D");
+		lv.insertLast("B");
+		lv.insertLast("A");
+		lv.insertLast("C");
+		lv.insertLast("B");
+		lv.insertLast("A");
+		Assert.assertEquals(lv.toString(), listaConElems.reverse().toString());
+	}
+	
+	@Test
+    public void testMaxRepeated() {
         lv.insertFirst("A");
         lv.insertLast("B");
         lv.insertLast("C");
@@ -118,9 +381,226 @@ public void antesDe() {
         lv.insertLast("O");
         lv.insertLast("Q");
         lv.insertLast("A");
-        
         Assert.assertEquals(4,lv.maxRepeated());
-        
     }
-
+	
+	@Test (expected = NullPointerException.class)
+	public void testIsEqualsNullExcep() {
+		lv=null;
+		listaConElems.isEquals(lv);
+	}
+	
+	@Test
+	public void testIsEquals() {
+		lv.insertFirst("D");
+		Assert.assertFalse(lv.isEquals(listaConElems));
+		lv.insertFirst("B");
+		lv.insertFirst("A");
+		lv.insertFirst("C");
+		lv.insertFirst("B");
+		lv.insertFirst("A");
+		Assert.assertTrue(lv.isEquals(listaConElems));
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testContainsAllsNullExcep() {
+		lv=null;
+		listaConElems.containsAll(lv);
+	}
+	
+	@Test
+	public void testContainsAll() {
+		lv.insertFirst("A");
+		lv.insertFirst("D");
+		Assert.assertTrue(listaConElems.containsAll(lv));
+		lv.insertFirst("W");
+		Assert.assertFalse(listaConElems.containsAll(lv));
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testIsSubListNullExcep() {
+		lv=null;
+		listaConElems.isSubList(lv);
+	}
+	
+	@Test
+	public void testIsSubList() {
+		lv.insertFirst("D");
+		lv.insertFirst("B");
+		lv.insertFirst("A");
+		Assert.assertTrue(listaConElems.isSubList(lv));
+		lv.insertFirst("B");
+		Assert.assertFalse(listaConElems.isSubList(lv));
+		Assert.assertTrue(listaConElems.isSubList(listaConElems));
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testToStringFromUntilFromIllegalExcep() {
+		listaConElems.toStringFromUntil(-1, 2);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testToStringFromUntilUntilIllegalExcep() {
+		listaConElems.toStringFromUntil(2, -7);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testToStringFromUntilUntilMayor() {
+		listaConElems.toStringFromUntil(3, 1);
+	}
+	
+	@Test
+	public void testToStringFromUntil() {
+		Assert.assertEquals("(A B C )", listaConElems.toStringFromUntil(1, 3));
+		Assert.assertEquals("(B C A )", listaConElems.toStringFromUntil(2, 4));
+		Assert.assertEquals("(B D )", listaConElems.toStringFromUntil(5, 6));
+		Assert.assertEquals("(A )", listaConElems.toStringFromUntil(1, 1));
+	}
+	
+	@Test
+	public void testToString() {
+		Assert.assertEquals("(A B C A B D )", listaConElems.toString());
+	}
+	
+	@Test (expected = NoSuchElementException.class)
+	public void testIteratorNextNoSuchElemExcep() {
+		lv.insertFirst("A");
+		Iterator <String> lista = lv.iterator();
+		lista.next();
+		lista.next();
+	}
+	
+	@Test (expected = NoSuchElementException.class)
+	public void testReverseIteratorNextNoSuchElemExcep() {
+		lv.insertFirst("A");
+		Iterator <String> lista = lv.reverseIterator();
+		lista.next();
+		lista.next();
+	}
+	
+	@Test (expected = NoSuchElementException.class)
+	public void testEvenPosIteratorNextNoSuchElemExcep() {
+		lv.insertFirst("A");
+		lv.insertFirst("A");
+		lv.insertFirst("A");
+		Iterator <String> lista = lv.evenPositionsIterator();
+		lista.next();
+		lista.next();
+	}
+	
+	@Test (expected = NoSuchElementException.class)
+	public void testProgressIteratorNextNoSuchElemExcep() {
+		lv.insertFirst("A");
+		lv.insertFirst("B");
+		lv.insertFirst("C");
+		lv.insertFirst("D");
+		lv.insertFirst("E");
+		lv.insertFirst("F");
+		lv.insertFirst("G");
+		Iterator <String> lista = lv.progressIterator();
+		lista.next();
+		lista.next();
+		lista.next();
+		lista.next();
+		lista.next();
+	}
+	
+	@Test
+	public void testIteratorNext() {
+		lv.insertFirst("A");
+		lv.insertFirst("B");
+		lv.insertFirst("C");
+		Iterator <String> lista = lv.iterator();
+		Assert.assertEquals("C", lista.next());
+		Assert.assertEquals("B", lista.next());
+		Assert.assertEquals("A", lista.next());
+	}
+	
+	@Test
+	public void testReverseIteratorHasNext() {
+		lv.insertFirst("A");
+		lv.insertFirst("B");
+		lv.insertFirst("C");
+		Iterator <String> lista = lv.reverseIterator();
+		Assert.assertTrue(lista.hasNext());
+		lista.next();
+		lista.next();
+		lista.next();
+		Assert.assertFalse(lista.hasNext());
+	}
+	
+	@Test
+	public void testReverseIteratorNext() {
+		lv.insertFirst("A");
+		lv.insertFirst("B");
+		lv.insertFirst("C");
+		Iterator <String> lista = lv.reverseIterator();
+		Assert.assertEquals("A", lista.next());
+		Assert.assertEquals("B", lista.next());
+		Assert.assertEquals("C", lista.next());
+	}
+	
+	@Test
+	public void testEvenPosIteratorHasNext() {
+		lv.insertFirst("A");
+		lv.insertFirst("B");
+		lv.insertFirst("C");
+		lv.insertFirst("D");
+		lv.insertFirst("E");
+		Iterator <String> lista = lv.evenPositionsIterator();
+		Assert.assertTrue(lista.hasNext());
+		lista.next();
+		Assert.assertTrue(lista.hasNext());
+		lista.next();
+		Assert.assertFalse(lista.hasNext());
+	}
+	
+	@Test
+	public void testEvenPosIteratorNext() {
+		lv.insertFirst("A");
+		lv.insertFirst("B");
+		lv.insertFirst("C");
+		lv.insertFirst("D");
+		lv.insertFirst("E");
+		lv.insertFirst("F");
+		Iterator <String> lista = lv.evenPositionsIterator();
+		Assert.assertEquals("E", lista.next());
+		Assert.assertEquals("C", lista.next());
+		Assert.assertEquals("A", lista.next());
+	}
+	
+	@Test
+	public void testProgressIteratorHasNext() {
+		lv.insertFirst("A");
+		lv.insertFirst("B");
+		lv.insertFirst("C");
+		lv.insertFirst("D");
+		lv.insertFirst("E");
+		lv.insertFirst("F");
+		lv.insertFirst("G");
+		Iterator <String> lista = lv.progressIterator();
+		Assert.assertTrue(lista.hasNext());
+		lista.next();
+		lista.next();
+		lista.next();
+		Assert.assertTrue(lista.hasNext());
+		lista.next();
+		Assert.assertFalse(lista.hasNext());
+	}
+	
+	@Test
+	public void testProgressIteratorNext() {
+		lv.insertFirst("A");
+		lv.insertFirst("B");
+		lv.insertFirst("C");
+		lv.insertFirst("D");
+		lv.insertFirst("E");
+		lv.insertFirst("F");
+		lv.insertFirst("G");
+		Iterator <String> lista = lv.progressIterator();
+		Assert.assertEquals("G", lista.next());
+		Assert.assertEquals("F", lista.next());
+		Assert.assertEquals("D", lista.next());
+		Assert.assertEquals("A", lista.next());
+	}
 }
--- src/ule/edi/doubleLinkedList/DoubleLinkedListImpl.java
+++ src/ule/edi/doubleLinkedList/DoubleLinkedListImpl.java
@@ -33,41 +33,119 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 
 	@SuppressWarnings("hiding")
 	private class DobleLinkedListIterator<T> implements Iterator<T> {
-		  // añadir atributos
-	 
 		
-       	
+		private DoubleNode<T> current;
+		
 		public DobleLinkedListIterator(DoubleNode<T> nodo) {
-			// todo
-			
-				}
+			this.current = nodo;
+		}
 		
 		@Override
 		public boolean hasNext() {
-			
-			// todo
-			return false;
+			return !(current==null);
 		}
 
 		@Override
 		public T next() {
-			// todo
-
-			return null;
-			
-				}
-
-		
+			if (! hasNext()) throw new NoSuchElementException();
+			T elemActual = current.elem;
+			current=current.next;
+			return elemActual;
+		}
 	}
 	
 	////// FIN ITERATOR
 	
 	
-	
-	/// TODO :  AÑADIR OTRAS CLASES PARA LOS OTROS 3 ITERADORES
-	
-	
-    /////
+///// ITERADOR reverse //////////
+
+	@SuppressWarnings("hiding")
+	private class DobleLinkedListReverseIterator<T> implements Iterator<T> {
+		
+		private DoubleNode<T> current;
+		
+		public DobleLinkedListReverseIterator(DoubleNode<T> nodo) {
+			this.current = nodo;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return !(current==null);
+		}
+		
+		@Override
+		public T next() {
+			if (! hasNext()) throw new NoSuchElementException();
+			T elemActual = current.elem;
+			current=current.prev;
+			return elemActual;
+		}
+	}
+		
+		////// FIN ITERATOR
+
+		
+	///// ITERADOR EvenPostions //////////
+
+		@SuppressWarnings("hiding")
+		private class DobleLinkedListEvenPostionsIterator<T> implements Iterator<T> {
+			
+			private DoubleNode<T> current;
+			
+			public DobleLinkedListEvenPostionsIterator(DoubleNode<T> nodo) {
+				this.current = nodo;
+			}
+			
+			@Override
+			public boolean hasNext() {
+				return !(current==null);
+			}
+			
+			@Override
+			public T next() {
+				if (! hasNext()) throw new NoSuchElementException();
+				T elemActual = current.elem;
+				if(current.next!=null) current=current.next.next;
+				else current=current.next;
+				return elemActual;
+			}
+		}
+			
+			////// FIN ITERATOR
+			
+		///// ITERADOR progress //////////
+
+			@SuppressWarnings("hiding")
+			private class DobleLinkedListProgressIterator<T> implements Iterator<T> {
+				
+				private DoubleNode<T> current;
+				private int jump;
+				
+				public DobleLinkedListProgressIterator(DoubleNode<T> nodo) {
+					this.current = nodo;
+					this.jump=0;
+				}
+				
+				@Override
+				public boolean hasNext() {
+					return !(current==null);
+				}
+				
+				@Override
+				public T next() {
+					if (! hasNext()) throw new NoSuchElementException();
+					T elemActual = current.elem;
+					for(int i=0;i<=this.jump;i++) {
+							if(current!=null) current= current.next;
+					}
+					this.jump++;
+					return elemActual;
+				}
+			}
+				
+				////// FIN ITERATOR
+				
+/////
 	
 	@SafeVarargs
 	public DoubleLinkedListImpl(T...v ) {
@@ -79,14 +157,14 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 
 	@Override
 	public boolean isEmpty() {
-		return (front==null && last==null);
+		return (front==null);
 	}
 
 
 	@Override
 	public void clear() {
-		front.prev=null;
-		last.next=null;
+		front=null;
+		last=null;
 	}
 
 
@@ -120,7 +198,7 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 
 	@Override
 	public T removeFirst() throws EmptyCollectionException{
-		if(isEmpty()) throw new EmptyCollectionException("COLA VACIA");
+		if(isEmpty()) throw new EmptyCollectionException(this.toString());
 		DoubleNode<T> obj = new DoubleNode<T>(front.elem);
 		front=front.next;
 		front.prev=null;
@@ -143,16 +221,17 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 		if(elem==null) throw new NullPointerException();
 		if(position<=0) throw new IllegalArgumentException();
 		if (position>size()) insertLast(elem);
+		else if(position==1) insertFirst(elem);
 		else {
 			DoubleNode<T> aux = front;
 			int posCounter=1;
 			while(aux!=null) {
 				if(posCounter==position) {
 					DoubleNode<T> obj = new DoubleNode<T>(elem);
+					aux.prev.next=obj;
+					obj.prev=aux.prev;
 					aux.prev=obj;
 					obj.next=aux;
-					obj.prev=aux.prev;
-					aux.prev.next=obj;
 					aux=last;
 				}
 				posCounter++;
@@ -170,10 +249,13 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 		while(aux!=null) {
 			if(aux.elem==target) {
 				DoubleNode<T> obj = new DoubleNode<T>(elem);
-				aux.prev=obj;
-				obj.next=aux;
-				obj.prev=aux.prev;
-				aux.prev.next=obj;
+				if(aux==front) insertFirst(elem);
+				else {
+					obj.prev=aux.prev;
+					aux.prev.next=obj;
+					aux.prev=obj;
+					obj.next=aux;
+				}
 				aux=last;
 			}
 			aux=aux.next;
@@ -184,7 +266,8 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 
 	@Override
 	public T getElemPos(int position) {
-		if(position<1 || position>size()) throw new IllegalArgumentException();
+		if(position<1 ) throw new IllegalArgumentException();
+		if(position>size()) throw new IllegalArgumentException();
 		DoubleNode<T> obj = new DoubleNode<T>(null);
 		DoubleNode<T> aux = front;
 		int posCounter=1;
@@ -193,6 +276,7 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 				obj.elem = aux.elem;
 				aux=last;
 			}
+			posCounter++;
 			aux=aux.next;
 		}
 		return obj.elem;
@@ -218,10 +302,10 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 	public int getPosLast(T elem) {
 		if(elem==null) throw new NullPointerException();
 		if(!contains(elem)) throw new NoSuchElementException();
-		int pos=0;
+		int pos=size()+1;
 		DoubleNode<T> aux = last;
 		while(aux!=null) {
-			pos++;
+			pos--;
 			if(elem==aux.elem) aux=front;
 			aux=aux.prev;
 		}
@@ -231,18 +315,35 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 
 	@Override
 	public T removePos(int pos) {
-		if(pos<1 || pos>size()) throw new IllegalArgumentException();
-		int posCounter=0;
+		if(pos<1) throw new IllegalArgumentException();
+		if(pos>size()) throw new IllegalArgumentException();
+		int posCounter=1;
 		DoubleNode<T> obj = new DoubleNode<T>(null);
 		DoubleNode<T> aux = front;
+		if(pos==1) {
+			if(size()==1) {
+				obj=aux;
+				clear();
+			}
+			else {
+				front=front.next;
+				front.prev=null;
+			}
+			return obj.elem;
+		}
+		if(pos==size()) {
+			last=last.prev;
+			last.next=null;		
+			return obj.elem;
+		}
 		while(aux!=null) {
-			posCounter++;
 			if(pos==posCounter) {
 				obj=aux;
 				aux.prev.next=aux.next;
 				aux.next.prev=aux.prev;
 				aux=last;
 			}
+			posCounter++;
 			aux=aux.next;
 		}
 		return obj.elem;
@@ -257,8 +358,21 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 		DoubleNode<T> aux = front;
 		while(aux!=null) {
 			if(elem==aux.elem) {
-				aux.prev.next=aux.next;
-				aux.next.prev=aux.prev;
+				if(size()==1) clear();
+				else {
+					if (aux==front) {
+						front=aux.next;
+						front.prev=null;
+					}
+					else if(aux==last) {
+						last=aux.prev;
+						last.next=null;
+					}
+					else{
+						aux.prev.next=aux.next;
+						aux.next.prev=aux.prev;
+					}
+				}
 				removeCounter++;
 			}
 			aux=aux.next;
@@ -346,12 +460,12 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 	@Override
 	public boolean containsAll(DoubleList<T> other) {
 		if(other==null)throw new NullPointerException();
-		DoubleNode<T> aux = front;
 		T elemPivote;
-		while(aux!=null) {
-			elemPivote=aux.elem;
-			if(!(other.contains(elemPivote))) return false;
-			aux=aux.next;
+		int posCounter=1;
+		while(other.size()>=posCounter) {
+			elemPivote=other.getElemPos(posCounter);
+			if(!(this.contains(elemPivote))) return false;
+			posCounter++;
 		}
 		return true;
 	}
@@ -360,19 +474,23 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 	@Override
 	public boolean isSubList(DoubleList<T> other) {
 		if(other==null)throw new NullPointerException();
-		if (this.toString().contains(other.toString())) return true;
+		if (this.toString().substring(1, this.toString().lastIndexOf(")")).contains(other.toString().substring(1, other.toString().lastIndexOf(")"))))	
+			return true;
 		return false;
 	}
 
 
 	@Override
 	public String toStringFromUntil(int from, int until) {
-		if(from<=0||until<=0||until<from)throw new IllegalArgumentException();
+		if(from<=0)throw new IllegalArgumentException();
+		if(until<=0)throw new IllegalArgumentException();
+		if(until<from)throw new IllegalArgumentException();
 		int posCounter=1;
 		StringBuffer output= new StringBuffer("(");
 		DoubleNode<T> aux = front;
 		while(aux!=null) {
 			if(posCounter>=from && posCounter<=until) output.append(aux.elem.toString() + " ");
+			posCounter++;
 			aux=aux.next;
 		}
 		output.append(")");
@@ -393,28 +511,25 @@ public class DoubleLinkedListImpl<T> implements DoubleList<T> {
 
 	@Override
 	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new DobleLinkedListIterator<T>(this.front);
 	}
 
 	@Override
 	public Iterator<T> reverseIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new DobleLinkedListReverseIterator<T>(this.last);
 	}
 
 
 	@Override
 	public Iterator<T> evenPositionsIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		DoubleNode<T> aux = front.next;
+		return new DobleLinkedListEvenPostionsIterator<T>(aux);
 	}
 
 
 	@Override
 	public Iterator<T> progressIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new DobleLinkedListProgressIterator<T>(this.front);
 	}
 
 
